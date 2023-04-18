4a3f5db17800e294bb99ff87781effa4e13c5aed
CaroliSampaio <caroline_08022@hotmail.com>
2023-04-17 23:03:59 -0300

    Add solução alternativa + otimização .

--- ex016.py
+++ ex016.py
@@ -1,5 +1,7 @@
 from math import trunc
+num_real = float(input('Digite um valor: '))
+print(f'O valor digitado foi {num_real} e sua porção inteira é {trunc(num_real)}.')
 
-num_real = float(input('Digite um número Real: '))
-num_int = trunc(num_real)
-print(f'A parte inteira do número real {num_real} é igual a {num_int}.')
+# Outra possibilidade sem importar biblioteca
+# num_real = float(input('Digite um valor: '))
+# print(f'O valor digitado foi {num_real} e sua porção inteira é {int(num_real)}.')

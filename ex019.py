90afa2e14ad958fe683f1f7ddcd00187f93856be
CaroliSampaio <caroline_08022@hotmail.com>
2023-04-17 23:49:40 -0300

    Add ex com o módulo random e função choice.

---
+++ ex019.py
@@ -0,0 +1,8 @@
+from random import choice
+aluno1 = input('Primeiro aluno: ')
+aluno2 = input('Segundo aluno: ')
+aluno3 = input('Terceiro aluno: ')
+aluno4 = input('Quarto aluno: ')
+lista = [aluno1, aluno2, aluno3, aluno4]
+sorteio = choice(lista)
+print(f'O aluno escolhido é {sorteio}')

30a03ec8e70c1daced259592363c28f0bb2a5630
CaroliSampaio <caroline_08022@hotmail.com>
2023-05-11 15:20:56 -0300

    Add ex Contagem regressiva

---
+++ ex046.py
@@ -0,0 +1,18 @@
+from time import sleep
+print('*-'*15)
+print('CONTAGEM REGRESSIVA PARA 2024')
+print('*-'*15)
+for i in range(10, 0, -1):
+    sleep(1)
+    print(i)
+sleep(1)
+print('''*         *
+
+
+
+         HAPPY            *
+ *             NEW            *
+
+         YEAR!            *
+*         *          *
+''')
